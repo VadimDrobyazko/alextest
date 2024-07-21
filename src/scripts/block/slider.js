5ded3c2d996d7fbb9b4b6584d0a11e020e9d2c6d
Вадим Дробязко <volandemortuwka@gmail.com>
2024-07-21 12:37:40 +0300

    fix

--- src/scripts/block/slider.js
+++ src/scripts/block/slider.js
@@ -1,50 +1,82 @@
 "use strict";
 
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 1,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-section-next",
-    prevEl: ".swiper-section-prev",
-  },
-  scrollbar: {
-    el: ".swiper-scroll",
-    draggable: true,
-  },
-  breakpoints: {
-    375: {
-      spaceBetween: 20,
+import Swiper from "swiper";
+import { Autoplay, Navigation, Scrollbar } from "swiper/modules";
+
+const swiperElArr = document.querySelectorAll(".swiper");
+if (swiperElArr.length) {
+  swiperElArr.forEach((swiperEl) => {
+    const swiper = new Swiper(swiperEl, {
+      modules: [Autoplay, Navigation, Scrollbar],
       slidesPerView: 1,
-      loop: true,
-    },
-    451: {
       spaceBetween: 20,
-      slidesPerView: 1.5,
-    },
-    651: {
-      spaceBetween: 20,
-      slidesPerView: 2,
-    },
-    901: {
-      spaceBetween: 16,
-      slidesPerView: 2,
-    },
-    1025: {
-      spaceBetween: 20,
-      slidesPerView: 2,
-    },
-    1200: {
-      spaceBetween: 20,
-      slidesPerView: 2,
-      loop: false,
-    },
-    1700: {
-      spaceBetween: 20,
-      slidesPerView: 3,
-    },
-  },
-});
+      navigation: {
+        nextEl: ".swiper-section-next",
+        prevEl: ".swiper-section-prev",
+      },
+      scrollbar: {
+        el: ".swiper-scroll",
+        draggable: true,
+      },
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      },
+      breakpoints: {
+        375: {
+          spaceBetween: 20,
+          slidesPerView: 1,
+          loop: true,
+        },
+        451: {
+          spaceBetween: 20,
+          slidesPerView: 1.5,
+        },
+        651: {
+          spaceBetween: 20,
+          slidesPerView: 2,
+        },
+        901: {
+          spaceBetween: 16,
+          slidesPerView: 2,
+        },
+        1025: {
+          spaceBetween: 20,
+          slidesPerView: 2,
+        },
+        1200: {
+          spaceBetween: 20,
+          slidesPerView: 2,
+          loop: false,
+        },
+        1700: {
+          spaceBetween: 20,
+          slidesPerView: 3,
+        },
+      },
+      on: {
+        init: function () {
+          updateScrollbarWidth(swiper);
+        },
+        resize: function () {
+          updateScrollbarWidth(swiper);
+        },
+        slideChange: function () {
+          updateScrollbarWidth(swiper);
+        },
+      },
+    });
 
-swiperObserve(swiper);
+    function updateScrollbarWidth(swiper) {
+      const totalSlides = swiper.slides.length;
+      const slidesPerView = swiper.params.slidesPerView;
+      const scrollbar = swiper.scrollbar.el;
+      const drag = scrollbar.querySelector(".swiper-scrollbar-drag");
+      const scrollbarWidth = scrollbar.offsetWidth;
 
-export default swiper;
+      const dragWidth = scrollbarWidth * (slidesPerView / totalSlides);
+      drag.style.width = `${dragWidth}px`;
+    }
+  });
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import form from "./block/form.js";
-import card from "./block/card.js";
-import nav from "./block/nav.js";
-import swiper from "./block/slider.js";
+import "./block/form.js";
+import "./block/card.js";
+import "./block/nav.js";
+import "./block/slider.js";
